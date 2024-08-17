6c7d8840109d778c91bb44cc240a90ad88af9e45
nawal07 <nawalsidiq077@gmail.com>
2024-08-17 14:31:52 +0530

    RESPONSIVE_UPDATES

--- main.js
+++ main.js
@@ -11,6 +11,32 @@ document.getElementById('hideButton').addEventListener('click', function () {
 });
 
 document
+  .getElementById('showMenuResponsive')
+  .addEventListener('click', function () {
+    const newDashboard = document.getElementById('menuResponsive');
+    const dashboard = document.getElementById('dashboardResponsive');
+
+    dashboard.classList.remove('show');
+    dashboard.classList.add('hide');
+    newDashboard.classList.remove('hide');
+    newDashboard.classList.add('show');
+  });
+
+
+  document
+  .getElementById('hideMenuResponsive')
+  .addEventListener('click', function () {
+    const newDashboard = document.getElementById('menuResponsive');
+    const dashboard = document.getElementById('dashboardResponsive');
+
+    dashboard.classList.remove('hide');
+    dashboard.classList.add('show');
+    newDashboard.classList.remove('show');
+    newDashboard.classList.add('hide');
+  });
+
+
+document
   .getElementById('hideButton-toggle')
   .addEventListener('click', function () {
     const dashboard = document.getElementById('dashboard');
@@ -21,3 +47,44 @@ document
     }, 10);
     hideButtonToggle.classList.remove('show');
   });
+
+Array.prototype.forEach.call(
+  document.getElementsByClassName('expandPayment'),
+  function (element, index) {
+    element.addEventListener('click', function () {
+      const description = document.getElementsByClassName('paymentDescription');
+      const expandPayment = document.getElementsByClassName('expandPayment');
+      const shrinkPayment = document.getElementsByClassName('shrinkPayment');
+      const paymentRows = document.getElementsByClassName('paymentRows');
+
+      Array.prototype.forEach.call(description, function (desc, i) {
+        desc.style.display = 'none';
+        expandPayment[i].style.display = 'block';
+        shrinkPayment[i].style.display = 'none';
+        paymentRows[i].classList.remove('show');
+      });
+
+      description[index].style.display = 'block';
+      expandPayment[index].style.display = 'none';
+      shrinkPayment[index].style.display = 'block';
+      paymentRows[index].classList.add('show');
+    });
+  }
+);
+
+Array.prototype.forEach.call(
+  document.getElementsByClassName('shrinkPayment'),
+  function (element, index) {
+    element.addEventListener('click', function () {
+      const description = document.getElementsByClassName('paymentDescription');
+      const expandPayment = document.getElementsByClassName('expandPayment');
+      const shrinkPayment = document.getElementsByClassName('shrinkPayment');
+      const paymentRows = document.getElementsByClassName('paymentRows');
+
+      description[index].style.display = 'none';
+      expandPayment[index].style.display = 'block';
+      shrinkPayment[index].style.display = 'none';
+      paymentRows[index].classList.add('hide');
+    });
+  }
+);
